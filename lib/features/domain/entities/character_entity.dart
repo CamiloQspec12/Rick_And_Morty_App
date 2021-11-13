5176a3eb48343e872d3d3b89b8a6bbce4bf23832
Gregory Iscala <greko11maister@gmail.com>
2021-11-13 14:45:54 -0500

    Entidad del Personaje

--- lib/features/domain/entities/character_entity.dart
+++ lib/features/domain/entities/character_entity.dart
@@ -1,7 +1,43 @@
 import 'package:equatable/equatable.dart';
 
 class CharacterEntity extends Equatable {
-  @override
-  List<Object?> get props => [];
+  int? id;
+  String? name;
+  String? species;
+  String? type;
+  String? gender;
+  OriginEntity? origin;
+  LocationEntity? location;
+  String? image;
+  List<String>? episode;
+  String? url;
+  String? created;
 
+  CharacterEntity(
+      {this.id,
+      this.name,
+      this.species,
+      this.type,
+      this.gender,
+      this.origin,
+      this.location,
+      this.image,
+      this.episode,
+      this.url,
+      this.created});
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        species,
+        type,
+        gender,
+        origin,
+        location,
+        image,
+        episode,
+        url,
+        created
+      ];
 }
