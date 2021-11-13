6b58d57aa8dd49adecfe8349af78cabf97216396
Gregory Iscala <greko11maister@gmail.com>
2021-11-13 15:58:07 -0500

    Agregando el contrato al caso de uso

--- lib/features/domain/usecases/get_characters_use_case.dart
+++ lib/features/domain/usecases/get_characters_use_case.dart
@@ -2,12 +2,15 @@ import 'package:dartz/dartz.dart';
 import 'package:rick_and_morty/core/error/failures.dart';
 import 'package:rick_and_morty/core/usecases/usecase.dart';
 import 'package:rick_and_morty/features/domain/entities/character_entity.dart';
+import 'package:rick_and_morty/features/domain/repositories/character_repository.dart';
 
-class GetCharactersUseCase implements UseCase<List<CharacterEntity>, NoParams>{
+class GetCharactersUseCase implements UseCase<List<CharacterEntity>, NoParams> {
+  final CharacterRepository repository;
+
+  GetCharactersUseCase(this.repository);
+
   @override
   Future<Either<Failure, List<CharacterEntity>>> call(NoParams params) {
-    // TODO: implement call
-    throw UnimplementedError();
+    return repository.getCharacters();
   }
-  
 }
